6000e8a391b1bbc650fedfd27c9eac007c23ebaf
Quinn Liu <quinnliu@vt.edu>
2013-09-15 01:12:45 -0400

    Outlined arraylist data structure to be implemented

---
+++ src/DataStructures/ArrayList.java
@@ -0,0 +1,81 @@
+package DataStructures;
+
+/**
+ *
+ * @author Quinn Liu (quinnliu@vt.edu)
+ * @version Sep 13, 2013
+ */
+public class ArrayList<E> implements ListInterface<E> {
+
+    @Override
+    public void insert(E item) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void append(E item) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public E remove() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public void clear() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void moveToStart() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void moveToEnd() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean previous() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public boolean next() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public int length() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public int currentPosition() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public void moveCurrentNodeToPosition(int position) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public E getValue() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+}
