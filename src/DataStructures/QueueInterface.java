249a107e20d5688e3b37caf087376c67dfb26d72
Quinn Liu <quinnliu@vt.edu>
2013-09-15 01:11:17 -0400

    Added javadoc comments

--- src/DataStructures/QueueInterface.java
+++ src/DataStructures/QueueInterface.java
@@ -7,13 +7,28 @@ package DataStructures;
  * @version Sep 1, 2013
  */
 public interface QueueInterface<E> {
+    /**
+     * Add an item to the read of the queue.
+     */
     public void enqueue(E item);
 
+    /**
+     * Remove and return the item at the front of the queue.
+     */
     public E dequeue();
 
+    /**
+     * Return the item at the front of the queue.
+     */
     public E frontValue();
 
+    /**
+     * Return how many elements are in the queue.
+     */
     public int length();
 
+    /**
+     * Remove all the items in the queue.
+     */
     public void clear();
 }
