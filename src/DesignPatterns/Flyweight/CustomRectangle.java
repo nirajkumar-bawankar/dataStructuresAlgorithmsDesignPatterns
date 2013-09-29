41e1bfb91d2222df7ce4f9c28b530a40a7be7035
Quinn Liu <quinnliu@vt.edu>
2013-09-29 13:13:37 -0400

    added example of Flyweight design pattern with creating rectangles

---
+++ src/DesignPatterns/Flyweight/CustomRectangle.java
@@ -0,0 +1,35 @@
+package DesignPatterns.Flyweight;
+
+import java.awt.*;
+
+/**
+ * @version Sept 29, 2013
+ */
+public class CustomRectangle {
+    private Color color = Color.black;
+    private int x, y, x2, y2;
+
+    public CustomRectangle(Color color) {
+	this.color = color;
+    }
+
+    public void draw(Graphics g, int upperX, int upperY, int lowerX, int lowerY) {
+	g.setColor(this.color);
+	g.fillRect(upperX, upperY, lowerX, lowerY);
+    }
+
+    // original forces creation of a rectangle every time
+    public CustomRectangle(Color color, int upperX, int upperY, int lowerX,
+	    int lowerY) {
+	this.color = color;
+	this.x = upperX;
+	this.y = upperY;
+	this.x2 = lowerX;
+	this.y2 = lowerY;
+    }
+
+//    public void draw(Graphics g) {
+//    	g.setColor(color);
+//    	g.fillRect(x, y, x2, y2);
+//    }
+}
