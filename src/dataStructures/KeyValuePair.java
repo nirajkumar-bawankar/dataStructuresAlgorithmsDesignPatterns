04b105aa32476b9417ca3e6450fb58a7f1cdbf46
Quinn Liu <quinnliu@vt.edu>
2013-10-06 13:46:08 -0400

    removed ClassCast Exception and correclty make KeyValuePair class generic

--- src/dataStructures/KeyValuePair.java
+++ src/dataStructures/KeyValuePair.java
@@ -7,30 +7,73 @@ package dataStructures;
  * values in another array with keys are their fields for other purposes.
  *
  * @version Sep 13, 2013
+ * @param <Key>
+ * @param <Value>
  */
-public class KeyValuePair implements Comparable {
+public class KeyValuePair<Key extends Comparable<Key>, Value> implements
+	Comparable<KeyValuePair<Key, Value>> {
 
-    private Comparable key;
-    private Object value;
+    private final Key key;
+    private final Value value;
 
-    public KeyValuePair(Comparable key, Object value) {
+    /**
+     * Create a new KeyValuePair object.
+     *
+     * @param key
+     * @param value
+     */
+    public KeyValuePair(Key key, Value value) {
 	this.key = key;
 	this.value = value;
     }
 
-    public Comparable getKey() {
+    /**
+     * Values are disregarded for the purposes of comparison.
+     */
+    @Override
+    public int compareTo(KeyValuePair<Key, Value> item) {
+	if (this.key == null) {
+	    if (item.key == null) {
+		return 0;
+	    } else {
+		// arbitrarily decides to sort null keys first.
+		return -1;
+	    }
+	} else {
+	    return this.key.compareTo(item.key);
+	}
+    }
+
+    /**
+     * @return The key object.
+     */
+    public Key getKey() {
 	return this.key;
     }
 
-    public Object getValue() {
+    /**
+     * @return The value object.
+     */
+    public Value getValue() {
 	return this.value;
     }
 
     @Override
-    public int compareTo(Object item) throws ClassCastException {
-	if (!(item instanceof KeyValuePair)) {
-	    throw new ClassCastException("A KeyValuePair object was expected");
+    public boolean equals(Object other) {
+	if (!this.getClass().equals(other.getClass())) {
+	    return false;
 	}
-	return this.key.compareTo(((KeyValuePair) item).key);
+	@SuppressWarnings("rawtypes")
+	KeyValuePair item = (KeyValuePair) other;
+	return ((this.key == null && item.key == null) || this.key
+		.equals(item.key))
+		&& ((this.value == null && item.value == null) || this.value
+			.equals(item.value));
+    }
+
+    @Override
+    public int hashCode() {
+	return ((this.key == null) ? 0 : this.key.hashCode())
+		^ ((this.value == null) ? 0 : this.value.hashCode());
     }
 }
