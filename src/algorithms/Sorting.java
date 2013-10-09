7247ff02e15e11b3eea811ff920a5fa84b89a844
Quinn Liu <quinnliu@vt.edu>
2013-10-08 22:14:18 -0400

    implemented array based merge sort

--- src/algorithms/Sorting.java
+++ src/algorithms/Sorting.java
@@ -43,8 +43,8 @@ public class Sorting {
      * Comparisons: Best Case = O(n^2) Average Case = O(n^2) Worst Case = O(n^2)
      * Swaps: Best Case = O(n) Average Case = O(n) Worst Case = O(n)
      *
-     * Use this sorting algorithm when: 1) The cost of swapping 2 elements is
-     * very large such as when the records are long strings.
+     * Use this sorting algorithm when: 1) The cost of swapping 2 elements in
+     * your array is very large such as when the records are long strings.
      *
      * To learn more about insertion sort visit:
      * http://algoviz.org/OpenDSA/Books/CS3114PM/html/SelectionSort.html
@@ -60,6 +60,79 @@ public class Sorting {
 		}
 	    }
 	    swap(array, indexOfLargestValue, array.length - (i + 1));
+	}
+    }
+
+    /**
+     * Use this sorting algorithm when: 1) When space is not an issue as a merge
+     * sort of an array takes twice the amount of space as insertionSort and
+     * selectionSort. 2) When sorting a singly linked list since merging does
+     * not require random access to list elements but instead use a singly
+     * linked list based merge sort instead of this array based merge sort.
+     *
+     * The array to be sorted is repeatedly split in half until subarrays of
+     * size 1 are reached.These subarrays of size 1 are merged in order to be of
+     * size 2. Subarrays of size 2 are merged in order to become subarrays of
+     * size 4, and so on. This means the depth of the recursion is log n for n
+     * records. The first level of recursion can be thought of as n subarrays of
+     * size 1. This requires O(n) total steps for merging. The next level of
+     * recursion has n/2 subarrays of size 2 that again requires O(n) steps of
+     * merging, and so on. Overall, at each of the log(n) levels or recursion,
+     * O(n) merging is being done. Thus the cost of merge sort is O(n * log n)
+     * for worst, average, and best cases.
+     *
+     * To learn more about merge sort visit:
+     * http://algoviz.org/OpenDSA/Books/CS3114PM/html/Mergesort.html
+     *
+     * @param array
+     *            array to be sorted
+     * @param temperaryArray
+     *            used in recursive implementation to sort larger and larger
+     *            subarrays of array
+     * @param leftIndex
+     *            left most accessible index for subarray of array
+     * @param rightIndex
+     *            right most accessible index for subarray of array
+     */
+    public static void mergeSort(Comparable[] array,
+	    Comparable[] temperaryArray, int leftIndex, int rightIndex) {
+	if (leftIndex == rightIndex) {
+	    return; // the array has been recursively reduced to array.size()
+		    // subarrays
+	}
+	int middleIndex = (leftIndex + rightIndex) / 2;
+
+	// merge sort first half
+	mergeSort(array, temperaryArray, leftIndex, middleIndex);
+
+	// merge sort second half
+	mergeSort(array, temperaryArray, middleIndex + 1, rightIndex);
+
+	// copy array/subarray to temperaryArray
+	for (int i = leftIndex; i <= rightIndex; i++) {
+	    temperaryArray[i] = array[i];
+	}
+
+	// do the merge operation back to array
+	int currentIndexWithinLeftSubArray = leftIndex;
+	int currentIndexWithinRightSubArray = middleIndex + 1;
+	for (int currentIndex = leftIndex; currentIndex <= rightIndex; currentIndex++) {
+	    // if true then all elements in left subarray have been merged into
+	    // the array
+	    if (currentIndexWithinLeftSubArray == middleIndex + 1) {
+		array[currentIndex] = temperaryArray[currentIndexWithinRightSubArray++];
+	    } else if (currentIndexWithinRightSubArray > rightIndex) {
+		// if true then all elements in right subarray have been merged
+		// into the array
+		array[currentIndex] = temperaryArray[currentIndexWithinLeftSubArray++];
+	    } else if (temperaryArray[currentIndexWithinLeftSubArray]
+		    .compareTo(temperaryArray[currentIndexWithinRightSubArray]) < 0) {
+		// get smaller value in left subarray
+		array[currentIndex] = temperaryArray[currentIndexWithinLeftSubArray++];
+	    } else {
+		// get smaller or equal value in right subarray
+		array[currentIndex] = temperaryArray[currentIndexWithinRightSubArray++];
+	    }
 	}
     }
 
