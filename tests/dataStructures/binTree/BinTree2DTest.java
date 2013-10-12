54c760b2ca329694c0a8d5e2522434b6b109864c
Quinn Liu <quinnliu@vt.edu>
2013-10-12 18:06:52 -0400

    last commit with A B and C D switched in BinTree2DTest class

--- tests/dataStructures/binTree/BinTree2DTest.java
+++ tests/dataStructures/binTree/BinTree2DTest.java
@@ -17,6 +17,11 @@ public class BinTree2DTest extends junit.framework.TestCase {
 	this.binTree = new BinTree2D<Point, BinTreeNode>(0.0, 100.0, 0.0, 100.0);
     }
 
+    /**
+     * Please refer to the images at the 2 following links while trying to
+     * understand the bin tree insert method as it is very confusing: Figure 1 @:
+     * Figure 2 @:
+     */
     public void test_insert() {
 	this.binTree.insert(new Point(30, 70),
 		new BinTreeLeafNode<Point, String>(new Point(30, 70), "A"));
@@ -24,6 +29,59 @@ public class BinTree2DTest extends junit.framework.TestCase {
 	this.binTree.insert(new Point(10, 45),
 		new BinTreeLeafNode<Point, String>(new Point(10, 45), "B"));
 
+	assertTrue(this.binTree.getRootNode() instanceof BinTreeInternalNode<?>);
+	assertTrue(((BinTreeInternalNode<?>) this.binTree.getRootNode())
+		.getLeftChild() instanceof BinTreeInternalNode<?>);
+	assertTrue(((BinTreeInternalNode<?>) this.binTree.getRootNode())
+		.getRightChild() instanceof BinTreeEmptyNode<?>);
+
+	// assert element B was inserted at the correct leaf node position
+	assertEquals(
+		"\n====Bin Tree Leaf Node====\nkey: (x, y) = (10.0, 45.0)\n   element: B"
+			+ "\n==========================",
+		((BinTreeLeafNode<Point, ?>) ((BinTreeInternalNode<?>) ((BinTreeInternalNode<?>) this.binTree
+			.getRootNode()).getLeftChild()).getLeftChild())
+			.getElement().toString());
+
+	// assert element A was moved to the correct leaf node position
+	assertEquals(
+		"\n====Bin Tree Leaf Node====\nkey: (x, y) = (30.0, 70.0)\n   element: A"
+			+ "\n==========================",
+		((BinTreeLeafNode<Point, ?>) ((BinTreeInternalNode<?>) ((BinTreeInternalNode<?>) this.binTree
+			.getRootNode()).getLeftChild()).getRightChild())
+			.getElement().toString());
+
+	this.binTree.insert(new Point(55, 90),
+		new BinTreeLeafNode<Point, String>(new Point(55, 90), "C"));
+
+	// assert element C was inserted at the correct leaf node positions
+	assertEquals(
+		"\n====Bin Tree Leaf Node====\nkey: (x, y) = (55.0, 90.0)\n   element: C"
+			+ "\n==========================",
+		((BinTreeLeafNode<Point, ?>) ((BinTreeInternalNode<?>) this.binTree
+			.getRootNode()).getRightChild()).getElement()
+			.toString());
+
+	// assert element A & B did not move from their correct leaf node
+	// positions
+	assertEquals(
+		"\n====Bin Tree Leaf Node====\nkey: (x, y) = (10.0, 45.0)\n   element: B"
+			+ "\n==========================",
+		((BinTreeLeafNode<Point, ?>) ((BinTreeInternalNode<?>) ((BinTreeInternalNode<?>) this.binTree
+			.getRootNode()).getLeftChild()).getLeftChild())
+			.getElement().toString());
+	assertEquals(
+		"\n====Bin Tree Leaf Node====\nkey: (x, y) = (30.0, 70.0)\n   element: A"
+			+ "\n==========================",
+		((BinTreeLeafNode<Point, ?>) ((BinTreeInternalNode<?>) ((BinTreeInternalNode<?>) this.binTree
+			.getRootNode()).getLeftChild()).getRightChild())
+			.getElement().toString());
+
+	// now we are going to add last element D that will dramatically change
+	// rootNodes right subtree since it's (x, y) position is very close to
+	// element C's (x, y) position
+	this.binTree.insert(new Point(52, 65),
+		new BinTreeLeafNode<Point, String>(new Point(52, 65), "D"));
 	System.out.println(this.binTree.getRootNode().toString());
     }
 
