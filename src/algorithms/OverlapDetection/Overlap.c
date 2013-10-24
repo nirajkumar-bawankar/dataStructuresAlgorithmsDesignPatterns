76d1178a0dc6e0b90a1b10579c09e97642994d62
Quinn Liu <quinnliu@vt.edu>
2013-10-24 12:24:10 -0400

    removed honor code

--- src/algorithms/OverlapDetection/Overlap.c
+++ src/algorithms/OverlapDetection/Overlap.c
@@ -1,25 +1,5 @@
-// On my honor:
-// - I have not discussed the C language code in my program with
-// anyone other than my instructor or the teaching assistants
-// assigned to this course.
-// - I have not used C language code obtained from another student,
-// or any other unauthorized source, either modified or unmodified.
-// - If any C language code or documentation used in my program
-// was obtained from another source, such as a text book or course
-// notes, that has been clearly noted with a proper citation in
-// the comments of my program.
-// - I have not designed this program in such a way as to defeat or
-// interfere with the normal operation of the Curator System.
-// Huanqing Liu
-
-/** CS 2505 Spring 2013
- *  Supplied framework for Intersecting Rectangles project.  Your task is to
- *  complete the supplied code to satisfy the posted specification for this
- *  assignment.  The occurrences of "???" in the code below indicate places
- *  where you must complete the implementation.
- *
- *  Student:   Huanqing Liu
- *  PID:       quinnliu
+/**
+ * @author Quinn Liu (quinnliu@vt.edu)
  */
 #include <stdio.h>      // for I/O functions
 #include <stdlib.h>     // generally useful
