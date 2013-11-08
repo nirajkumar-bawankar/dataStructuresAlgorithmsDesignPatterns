2264445f0955d2bc4a59eff6509af37ce2dd18da
Quinn Liu <quinnliu@vt.edu>
2013-11-08 18:26:25 -0500

    changed tests for larger numbers

--- tests/algorithms/BinarySearchTest.java
+++ tests/algorithms/BinarySearchTest.java
@@ -10,7 +10,7 @@ public class BinarySearchTest extends junit.framework.TestCase {
     private int[] sortedArray;
 
     public void setUp() {
-	this.sortedArray = new int[10];
+	this.sortedArray = new int[100];
 	for (int i = 0; i < this.sortedArray.length; i++) {
 	    // sorted array contains numbers 0 through 9 inclusive
 	    this.sortedArray[i] = i;
@@ -19,8 +19,8 @@ public class BinarySearchTest extends junit.framework.TestCase {
 
     public void test_search() {
 	// value 10 is not in the sorted array
-	assertEquals(-1, BinarySearch.search(this.sortedArray, 10));
+	assertEquals(-1, BinarySearch.search(this.sortedArray, 100));
 
-	assertEquals(3, BinarySearch.search(this.sortedArray, 3));
+	assertEquals(69, BinarySearch.search(this.sortedArray, 69));
     }
 }
