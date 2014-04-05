80a645ed419b633bc36fa866386783b890e0acb9
Quinn Liu <quinnliu@vt.edu>
2014-04-05 18:51:06 -0400

    finished data fields and Entry class

--- src/dataStructures/hashTable/Hashtable.java
+++ src/dataStructures/hashTable/Hashtable.java
@@ -10,7 +10,32 @@ package dataStructures.hashTable;
 public class Hashtable<Key, Value> implements
 	KeyValueHashMapInterface<Key, Value> {
 
+    /**
+     * A hashtable with a prime number size has emprically been shown to have
+     * less collisions when probing.
+     */
+    private static final int INITIAL_CAPACITY = 101;
+
+    /**
+     * Load factor = # of filled cells divided by hashtable size. Load factor =
+     * 0.75 = average # of probes is 1.38 using chaining.
+     */
+    private double MAXIMUM_LOAD_FACTOR = 0.75;
+
     private Entry<Key, Value>[] hashtable;
+    private int numberOfKeys;
+    private int numberOfDeletes;
+
+    /**
+     * Used to indicate that the Entry at a particular element in the
+     * hashtable array has been deleted.
+     */
+    private final Entry<Key, Value> DELETED_ENTRY = new Entry<Key, Value>(null,
+	    null);
+
+    public Hashtable() {
+	this.hashtable = new Entry[this.INITIAL_CAPACITY];
+    }
 
     @Override
     public Value get(Object key) {
@@ -41,5 +66,4 @@ public class Hashtable<Key, Value> implements
 	// TODO Auto-generated method stub
 	return 0;
     }
-
 }
